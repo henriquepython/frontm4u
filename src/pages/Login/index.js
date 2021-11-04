ac5cc1f0b7665ca89dbf0d6a08e1c015d5b4d92d
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-11-04 17:41:28 -0300

    pagina login finalizada

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css'
 
 import { MdAlternateEmail, MdLock } from "react-icons/md";
+import { HiEye, HiEyeOff } from "react-icons/hi";
 
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [show, setShow] = useState(false)
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setShow(!show);
+    }
+
     return(
         <div className='login'>
             <div className='login-logo'>
@@ -19,17 +29,34 @@ const Login = () => {
                 <div className="login-loginInputEmail">
                     <MdAlternateEmail />
                     <input
-                    type="text"
+                    type="email"
                     placeholder="Digite um email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                     />
                 </div>
 
                 <div className="login-loginInputPassword">
                     <MdLock />
                     <input
-                    type="text"
                     placeholder="Digite sua senha"
+                    type={show ? "text" : "password"}
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                     />
+                    <div className="login-eye">
+                        {show ? (
+                            <HiEye
+                                size={20}
+                                onClick={handleClick}
+                                 />
+                        ) : (
+                            <HiEyeOff 
+                                size={20}
+                                onClick={handleClick}
+                                 />
+                        )}
+                    </div>
                 </div>
 
                 <button type="submit">
